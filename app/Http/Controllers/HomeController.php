877282058a46ac877d00c3e30e59c938da279034
Muhammad Khidhir Ibrahim <khidhir.ibrahim@outlook.com>
2022-10-18 22:26:05 +0700

    Update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,7 +15,7 @@ class HomeController extends Controller
     public function cekTagihan(Request $request, $id){
         $masjid = Masjid::find($id);
         $ch = curl_init();
-
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_URL, 'https://api.sepulsa.com/api/v1/carts/add/');
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_POST, 1);
